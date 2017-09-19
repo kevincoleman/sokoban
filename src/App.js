d668a2225f8b06999d70345507b72b4de9fc87ec
Kevin Coleman <kevin.coleman@k3integrations.com>
2017-09-19 14:38:03 -0700

    Mock level to test display

--- src/App.js
+++ src/App.js
@@ -2,33 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+
   render() {
+
+    var level1 = {
+      width: 4,
+      height: 4,
+      index: [
+        "brick", "brick", "brick", "brick",
+        "brick", "",      "box",   "brick",
+        "brick", "",      "",      "brick",
+        "brick", "brick", "brick", "brick"
+      ]
+    }
+
+   let displayLevel = function(level) {
+      return level.index.map((type, index) => {
+        return <div
+          className={"block " + type}
+          style={{gridColumn: (index % level.width) + 1, gridRow: (index + 1) / level.height}}
+        >
+        </div>
+      });
+    }
+
     return (
       <div className="App">
-        <div
-          className="block brick"
-          style={{gridColumn: '2', gridRow: '1'}}
-        ></div>
-        <div
-          className="block diamond"
-          style={{gridColumn: '2', gridRow: '2'}}
-        ></div>
-        <div
-          className="block box"
-          style={{gridColumn: '3', gridRow: '2'}}
-        ></div>
-        <div
-          className="block brick"
-          style={{gridColumn: '4', gridRow: '2'}}
-        ></div>
-        <div
-          className="block diamond"
-          style={{gridColumn: '2', gridRow: '3'}}
-        ></div>
-        <div
-          className="block brick"
-          style={{gridColumn: '4', gridRow: '4'}}
-        ></div>
+        { displayLevel(level1) }
       </div>
     );
   }
