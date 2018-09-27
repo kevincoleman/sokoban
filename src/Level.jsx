38bfa0c08f39fe04f976ed77bbfdb66fe89e3761
kevincoleman <kevin.n.coleman@gmail.com>
2018-09-27 16:08:40 -0700

    Move mocked level to another file

--- src/Level.jsx
+++ src/Level.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./Level.css";
 import Footer from "./Footer";
 import Block from "./Block";
+import levels from "./levels.js";
 
 class Level extends Component {
   constructor(props) {
@@ -9,70 +10,25 @@ class Level extends Component {
     this.state = {
       moves: 0,
       pushes: 0,
-      time: "00:00:00"
+      time: "00:00:00",
+      level: levels.filter(level => props.level == level.number)[0]
     };
   }
 
   render() {
-    let data = {
-      number: 1,
-      levelMap: {
-        width: 7,
-        height: 5,
-        grid: [
-          { type: "brick", x: 1, y: 1 },
-          { type: "brick", x: 2, y: 1 },
-          { type: "brick", x: 3, y: 1 },
-          { type: "brick", x: 4, y: 1 },
-          { type: "brick", x: 5, y: 1 },
-          { type: "brick", x: 6, y: 1 },
-          { type: "brick", x: 7, y: 1 },
-          { type: "brick", x: 1, y: 2 },
-          { type: "blank", x: 2, y: 2 },
-          { type: "blank", x: 3, y: 2 },
-          { type: "blank", x: 4, y: 2 },
-          { type: "blank", x: 5, y: 2 },
-          { type: "blank", x: 6, y: 2 },
-          { type: "brick", x: 7, y: 2 },
-          { type: "brick", x: 1, y: 3 },
-          { type: "blank", x: 2, y: 3 },
-          { type: "diamond", x: 3, y: 3 },
-          { type: "box", x: 4, y: 3 },
-          { type: "man", x: 5, y: 3 },
-          { type: "blank", x: 6, y: 3 },
-          { type: "brick", x: 7, y: 3 },
-          { type: "brick", x: 1, y: 4 },
-          { type: "blank", x: 2, y: 4 },
-          { type: "blank", x: 3, y: 4 },
-          { type: "blank", x: 4, y: 4 },
-          { type: "blank", x: 5, y: 4 },
-          { type: "blank", x: 6, y: 4 },
-          { type: "brick", x: 7, y: 4 },
-          { type: "brick", x: 1, y: 5 },
-          { type: "brick", x: 2, y: 5 },
-          { type: "brick", x: 3, y: 5 },
-          { type: "brick", x: 4, y: 5 },
-          { type: "brick", x: 5, y: 5 },
-          { type: "brick", x: 6, y: 5 },
-          { type: "brick", x: 7, y: 5 }
-        ]
-      }
-    };
-
-    // add ids before rendering
-    data.levelMap.grid.forEach((block, index) => {
+    // add ids for indexing
+    this.state.level.chart.grid.forEach((block, index) => {
       block.id = index;
     });
-
     return (
       <div className="Level">
         <div
-          className="levelMap"
+          className="chart"
           style={{
-            width: data.levelMap.width * 64
+            width: this.state.level.chart.width * 64
           }}
         >
-          {data.levelMap.grid.map(block => (
+          {this.state.level.chart.grid.map(block => (
             <Block type={block.type} x={block.x} y={block.y} key={block.id} />
           ))}
         </div>
---
+++ src/levels.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    number: 1,
+    chart: {
+      width: 7,
+      height: 5,
+      grid: [
+        { type: "brick", x: 1, y: 1 },
+        { type: "brick", x: 2, y: 1 },
+        { type: "brick", x: 3, y: 1 },
+        { type: "brick", x: 4, y: 1 },
+        { type: "brick", x: 5, y: 1 },
+        { type: "brick", x: 6, y: 1 },
+        { type: "brick", x: 7, y: 1 },
+        { type: "brick", x: 1, y: 2 },
+        { type: "blank", x: 2, y: 2 },
+        { type: "blank", x: 3, y: 2 },
+        { type: "blank", x: 4, y: 2 },
+        { type: "blank", x: 5, y: 2 },
+        { type: "blank", x: 6, y: 2 },
+        { type: "brick", x: 7, y: 2 },
+        { type: "brick", x: 1, y: 3 },
+        { type: "blank", x: 2, y: 3 },
+        { type: "diamond", x: 3, y: 3 },
+        { type: "box", x: 4, y: 3 },
+        { type: "man", x: 5, y: 3 },
+        { type: "blank", x: 6, y: 3 },
+        { type: "brick", x: 7, y: 3 },
+        { type: "brick", x: 1, y: 4 },
+        { type: "blank", x: 2, y: 4 },
+        { type: "blank", x: 3, y: 4 },
+        { type: "blank", x: 4, y: 4 },
+        { type: "blank", x: 5, y: 4 },
+        { type: "blank", x: 6, y: 4 },
+        { type: "brick", x: 7, y: 4 },
+        { type: "brick", x: 1, y: 5 },
+        { type: "brick", x: 2, y: 5 },
+        { type: "brick", x: 3, y: 5 },
+        { type: "brick", x: 4, y: 5 },
+        { type: "brick", x: 5, y: 5 },
+        { type: "brick", x: 6, y: 5 },
+        { type: "brick", x: 7, y: 5 }
+      ]
+    }
+  }
+];
