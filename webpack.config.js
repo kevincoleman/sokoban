24a26c0d4009c7089b820a0a92bc7d6923419d42
kevincoleman <kevin.n.coleman@gmail.com>
2018-10-04 13:49:48 -0700

    Set up two webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,5 @@
 const path = require("path");
-const webpack = require("webpack");
 
-module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["env"] }
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
-  output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    historyApiFallback: true,
-    hotOnly: true
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+module.exports = env => {
+  return require(`./webpack.${env}.js`);
 };
